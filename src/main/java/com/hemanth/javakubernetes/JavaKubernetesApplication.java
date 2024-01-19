24cef82052eb2106d02a7a39dab9469f90f68488
hemanthcse1 <hemanthcse1@gmail.com>
2024-01-19 21:05:15 +0530

    Initial commit

---
+++ src/main/java/com/hemanth/javakubernetes/JavaKubernetesApplication.java
@@ -0,0 +1,13 @@
+package com.hemanth.javakubernetes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaKubernetesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaKubernetesApplication.class, args);
+    }
+
+}
